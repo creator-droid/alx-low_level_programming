5119b8fce05fb821830bde654de5fa23d343c1fe
Nomfanelo <sihle.mkhonza98@gmail.com>
2023-04-11 16:08:52 -0700

    Dated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 		i++;
 	while (s2[str] != '\0')
-		ci++;
+		str++;
 	concat = malloc(sizeof(char) * (i + str + 1));
 	if (concat == NULL)
 		return (NULL);

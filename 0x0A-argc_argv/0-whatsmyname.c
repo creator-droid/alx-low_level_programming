92b60f19fb1f39196feb6c63a634477c019bc2c7
creator-droid <106113368+creator-droid@users.noreply.github.com>
2023-05-02 14:34:15 +0200

    Delete 0x0A-argc_argv directory

--- 0x0A-argc_argv/0-whatsmyname.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * 0-whatsmyname.c -  a program that prints its name, followed by a new line.
- */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-	printf("%s\n", argv[0]);
-	return(0);
-}
--- 0x0A-argc_argv/1-args.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * 1-args.c - a program that prints the number of arguments passed into it
- */
-int main(int argc, char *argv[] __attribute__((unused)))
-{
-	printf("%d\n", argc - 1);
-	return(0);
-}
